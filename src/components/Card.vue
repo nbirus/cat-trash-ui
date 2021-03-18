df2904009654fffb5694eb01ed1abfefe520aa56
Nathan Birus <nbirus@harding.edu>
2021-03-17 20:17:50 -0500

    first commit

---
+++ src/components/Card.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="card">
+    <div class="card__img">
+      <img :src="imgSrc" :alt="`Image of ${label}`" />
+    </div>
+    <div class="card__info">
+      <div class="card__info-text">
+        <strong class="text" v-text="label"></strong>
+        <span class="text secondary" v-text="`$${price}`"></span>
+      </div>
+      <div class="card__info-action">
+        <button>SELECT</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "card",
+  props: ["id", "label", "imgSrc", "price"],
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  width: 100%;
+  height: 100%;
+  border: solid thin lightgray;
+  display: flex;
+  border-radius: 4px;
+  flex-direction: column;
+
+  &__img {
+    flex: 0 0 80%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  &__info {
+    height: 100%;
+    flex: 0 1 auto;
+    border-top: solid thin lightgray;
+    background-color: white;
+    display: flex;
+    align-items: center;
+    padding: 0 1.5rem;
+
+    &-text {
+      flex: 1 1 100%;
+      text-align: left;
+      display: flex;
+      flex-direction: column;
+
+      strong {
+        margin-bottom: 0.35rem;
+        font-size: 1em;
+      }
+      span {
+        font-size: 0.85em;
+      }
+    }
+    &-action {
+      flex: 0 0 auto;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,186 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="page">
+    <div class="page__header">
+      <v-image
+        class="page__header-img"
+        src="cat.jpg"
+        size="cover"
+        style="height: 50vh"
+      ></v-image>
+      <div class="page__header-content">
+        <h1>Cat Trash</h1>
+        <p>Providing the highest quality trash for your cats needs</p>
+        <button @click="checkout" type="button">SHOP TRASH</button>
+      </div>
+    </div>
+    <div class="page__body">
+      <ul class="page__cards">
+        <li class="page__cards-card" v-for="(item, i) in items" :key="i">
+          <card v-bind="item" />
+        </li>
+      </ul>
+    </div>
+    <div class="page__footer">
+      This website was made by Nathan Birus out of&nbsp;<i>spite</i>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Card from "@/components/Card";
+import VImage from "@/components/Image";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    Card,
+    VImage,
+  },
+  data() {
+    return {
+      items: [
+        {
+          id: "noodle",
+          label: "Dry Noodle",
+          imgSrc: "",
+          price: "0.99",
+        },
+        {
+          id: "hdmi-cover",
+          label: "HDMI Cover",
+          imgSrc: "",
+          price: "0.99",
+        },
+        {
+          id: "drywall-screw",
+          label: "Drywall Screw",
+          imgSrc: "",
+          price: "0.99",
+        },
+        {
+          id: "breadtie",
+          label: "Bread Tie",
+          imgSrc: "",
+          price: "0.99",
+        },
+        {
+          id: "pastic-hook",
+          label: "Pastic Hook",
+          imgSrc: "",
+          price: "0.99",
+        },
+        {
+          id: "loose-food",
+          label: "Loose Bits of Food",
+          imgSrc: "",
+          price: "0.99",
+        },
+        {
+          id: "string",
+          label: "String",
+          imgSrc: "",
+          price: "0.99",
+        },
+        {
+          id: "ribbon",
+          label: "Ribbon",
+          imgSrc: "",
+          price: "0.99",
+        },
+        {
+          id: "plastic-bag",
+          label: "Plastic Bag",
+          imgSrc: "",
+          price: "0.99",
+        },
+      ],
+    };
+  },
+  methods: {
+    checkout() {
+      fetch("http://localhost:3000/create-checkout-session", {
+        method: "POST",
+      })
+        .then(function (response) {
+          console.log(response);
+          return response.json();
+        })
+        .then(function (session) {
+          return stripe.redirectToCheckout({ sessionId: session.id });
+        })
+        .then(function (result) {
+          // If redirectToCheckout fails due to a browser or network
+          // error, you should display the localized error message to your
+          // customer using error.message.
+          if (result.error) {
+            alert(result.error.message);
+          }
+        })
+        .catch(function (error) {
+          console.error("Error:", error);
+        });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.page {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+
+  &__header {
+    flex: 0 0 50vh;
+    background-color: black;
+    position: relative;
+
+    &-img {
+      position: absolute;
+      top: 0px;
+      right: 0px;
+      bottom: 0px;
+      left: 0px;
+    }
+    &-content {
+      position: absolute;
+      top: 0px;
+      right: 0px;
+      bottom: 0px;
+      left: 0px;
+      display: flex;
+      justify-content: center;
+      align-items: flex-start;
+      flex-direction: column;
+      color: white;
+      max-width: 1000px;
+      margin: 0 auto;
+      padding: 0 1rem;
+    }
+  }
+  &__body {
+    flex: 0 1 100%;
+    padding: 5rem 3rem;
+  }
+  &__cards {
+    max-width: 1000px;
+    margin: 0 auto;
+    display: grid;
+    grid-gap: 2rem;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    padding: 0 1rem;
+
+    &-card {
+      width: 300px;
+      height: 350px;
+    }
+  }
+  &__footer {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 1rem;
+    font-size: 0.9em;
   }
 }
-</script>
+</style>
